a20d29fe41f09e70a8833413f104c7fb94f26f78
melm18 <melusimhlanga18@gmail.com>
2023-03-21 09:02:05 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,11 @@
  */
 void print_alphabet(void)
 {
-	char letterr;
+	char la;
 
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
 
 
 	_putchar('\n');
