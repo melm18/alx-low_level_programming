2fe54221dca14f5cc13abd0e6b65b1f6d5f00de4
melm18 <melusimhlanga18@gmail.com>
2023-04-12 01:47:02 -0700

    main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio.h>
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,4 +23,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN_H */
+#endif
