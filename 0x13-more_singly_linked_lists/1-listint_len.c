69c2a114b15c30b361a1dabb2fcc117e4a695b44
melm18 <melusimhlanga18@gmail.com>
2023-05-02 07:15:49 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,21 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the numbee in nodess
+ * listint_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * @h: head of a list in node
- *
- * Return: numbers of nodess.
+ * Return: number of nodes
  */
 size_t listint_len(const listint_t *h)
 {
-	size_t node = 0;
+	size_t num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		num++;
 		h = h->next;
-		node++;
 	}
-	return (node);
+
+	return (num);
 }
+
